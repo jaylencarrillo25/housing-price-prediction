a668aea677697a17ca763a041ff7d639142ad137
Jaylen Yomara Carrillo Godoy <jaylenc1@terpmail.umd.edu>
2025-04-25 18:39:24 -0400

    updated dataset.py

--- housing_predictor/dataset.py
+++ housing_predictor/dataset.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 def load_data(train_path='data/raw/train.csv', test_path='data/raw/test.csv'):
-    """Load training and testing datasets."""
+    """Loads training and testing datasets."""
     train_df = pd.read_csv(train_path)
     test_df = pd.read_csv(test_path)
     return train_df, test_df
