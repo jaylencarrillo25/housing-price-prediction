51ff0cde1f2f8930706ff1a7fe85dcd630135217
Jaylen Yomara Carrillo Godoy <jaylenc1@terpmail.umd.edu>
2025-04-25 22:41:13 -0400

    data

--- housing_predictor/dataset.py
+++ housing_predictor/dataset.py
@@ -1,7 +1,6 @@
 import pandas as pd
 
 def load_data(train_path='data/raw/train.csv', test_path='data/raw/test.csv'):
-    """Load training and testing datasets."""
     train_df = pd.read_csv(train_path)
     test_df = pd.read_csv(test_path)
     return train_df, test_df
--- test_imports.py
+++
@@ -1,3 +0,0 @@
-from housing_predictor.modeling.train import train_linear_regression, train_random_forest
-
-print("train_linear_regression and train_random_forest imported successfully!")
