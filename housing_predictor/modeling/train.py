f27c0da2daef6a5f0682206caba00a47690cc998
Jaylen Yomara Carrillo Godoy <jaylenc1@terpmail.umd.edu>
2025-04-26 13:31:35 -0400

    decision tree

--- housing_predictor/modeling/train.py
+++ housing_predictor/modeling/train.py
@@ -1,5 +1,7 @@
 from sklearn.linear_model import LinearRegression
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.tree import DecisionTreeRegressor
+
 
 def train_linear_regression(X, y):
     """Train a Linear Regression model."""
@@ -10,5 +12,10 @@ def train_linear_regression(X, y):
 def train_random_forest(X, y):
     """Train a random forest regressor."""
     model = RandomForestRegressor(random_state=42)
+    model.fit(X, y)
+    return model
+
+def train_decision_tree(X, y):
+    model = DecisionTreeRegressor(random_state=42)
     model.fit(X, y)
     return model
